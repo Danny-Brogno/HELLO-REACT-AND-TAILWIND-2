5b5c2968f13241ea293f191df2a578cbc30d4623
danny <danny.brogno@gmail.com>
2026-01-21 09:45:41 +0100

    added first filter(rating)

--- src/components/main.js
+++ src/components/main.js
@@ -4,34 +4,62 @@ import { SelectSection } from './sub-components/selectSection.jsx';
 
 export const Main = () => {
   
+  // API LOGIC
   const [movies, setMovies] = useState([]);
   useEffect(()=>{
     fetchMovies();
   },[])
+  
+  // FILTER LOGIC
+  const [allMoviesFiltered, setAllMoviesFiltered] = useState([]);
+  const [givingRating,setRating] = useState(0);
+  
+  // API CALL
   const fetchMovies = async()=>{  
     const res = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=5f2bcf7b4089e10f6f5653b724f0559a");
     const data = await res.json();
     console.log(data);
     setMovies(data.results);
+    setAllMoviesFiltered(data.results);
+  }
+  
+  // FILTER "CALL"
+  const handleFilter = (rating)=> {
+    if (rating == givingRating) {
+      setRating(0);
+      setMovies(allMoviesFiltered);
+    } else {
+      setRating(rating);
+      const filteredMovies = allMoviesFiltered.filter((movie) => movie.vote_average >= rating);
+      setMovies(filteredMovies);
+    }
   }
   
   return (
     <main className="main">
-      <SelectSection />
+      <SelectSection handleFilter={handleFilter} givingRating={givingRating}/>
       <div className="flex flex-row items-center justify-around flex-wrap mt-4 px-10">
-      {movies.map((movie)=>(
-          <Movie 
-            key = {movie.id}
-            id={movie.id}
-            movie = {movie}
-            title = {movie.title.slice(0,34)}
-            rating = {movie.vote_average.toFixed(1)}
-            description={movie.overview.slice(0,120)+".."}
-            image={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
-            releaseDate = {movie.release_date}
-            originalLanguage = {movie.original_language}
-          />
-      ))}
+        
+        {
+          movies.length > 0 ? (
+            movies.map((movie) => (
+              <Movie 
+                key={movie.id}
+                id={movie.id}
+                movie={movie}
+                title={movie.title.slice(0, 34)}
+                rating={movie.vote_average.toFixed(1)}
+                description={movie.overview.slice(0, 120) + ".."}
+                image={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
+                releaseDate={movie.release_date}
+                originalLanguage={movie.original_language}
+              />
+            ))
+          ) : (
+            <p>No movies found matching that rating.</p>
+          ) 
+        }
+
       </div>
     </main>
   );
--- src/components/sub-components/selectSection.jsx
+++ src/components/sub-components/selectSection.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
 
-export const SelectSection = () => {
-  return (
+export const SelectSection = ({ handleFilter, givingRating }) => {
+  return(
+    
     <div className="flex justify-center mt-10 h-[5em]">
-      
+
       <div className="flex flex-col justify-between mr-5">
-        <h3 className="cursor-pointer hover:text-blue-700 transition-all duration-300"> Select movies that have 8+ rating</h3>
-        <h3 className="cursor-pointer hover:text-blue-700 transition-all duration-300"> Select movies that have 7+ rating</h3>
-        <h3 className="cursor-pointer hover:text-blue-700 transition-all duration-300"> Select movies that have 6+ rating</h3>
+
+        <h3 
+          className={givingRating === 8 ? 'h3-element active cursor-pointer' : 'h3-element cursor-pointer hover:text-blue-700 transition-all duration-300'}
+          onClick={() => handleFilter(8)}
+          > 
+          Select movies that have 8+ rating
+        </h3>
+        
+        <h3 
+          className={givingRating === 7 ? 'h3-element active cursor-pointer' : 'h3-element cursor-pointer hover:text-blue-700 transition-all duration-300'}
+          onClick={() => handleFilter(7)}
+          > 
+          Select movies that have 7+ rating
+        </h3>
+        
+        <h3 
+          className={givingRating === 6 ? 'h3-element active cursor-pointer' : 'h3-element cursor-pointer hover:text-blue-700 transition-all duration-300'}
+          onClick={() => handleFilter(6)}
+          > 
+          Select movies that have 6+ rating
+        </h3>
+        
       </div>
       
       <select
@@ -15,24 +35,24 @@ export const SelectSection = () => {
         id=""
         defaultValue=""
         className="movie-sorting bg-purple-500 rounded p-4 h-full"
-      >
+        >
         <option value="" disabled hidden>Select an option</option>
         <option value="">Sort by</option>
         <option value="">Date</option>
         <option value="">Rating</option>
       </select>
-      
+
       <select
         name="sort"
         id="sort"
         defaultValue=""
         className="option bg-lime-500 rounded p-4 ml-4 h-full"
-      >
+        >
         <option value="" disabled hidden>Select an option</option>
         <option value="asc">Ascending</option>
         <option value="desc">Descending</option>
       </select>
-      
+
     </div>
   )
 }
